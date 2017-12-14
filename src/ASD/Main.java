180fd4d083f84812c90c6fef08e619d4c4fde449
Almoom <32739874+Almoom@users.noreply.github.com>
2017-12-14 22:49:36 +0500

    v2

--- src/ASD/Main.java
+++ src/ASD/Main.java
@@ -3,7 +3,7 @@ package ASD;
 public class Main {
 
     public static void main(String[] args) {
-	int[][] field = new int[4][4];
+	int[][] field = new int[7][8];
         for (int i = 0; i < field.length; i++) {
             for (int j = 0; j < field[i].length; j++) {
                 field[i][j] = 0;
@@ -12,37 +12,37 @@ public class Main {
             System.out.println();
         }
         System.out.println();
-        int y = 0;
-        int x = 0;
+        int y = 6;
+        int x = 7;
         field[y][x] = 1;
         boolean check = true;
         while (check){
-            if (y-2>=0 && x-1 >=0 && field[y-2][x-1] != 1){
-                y = y-2;
-                x = x-1;
+            if (y-1 >= 0 && x+2 < field[y].length && field[y-1][x+2] != 1){
+                y = y-1;
+                x = x+2;
+                field[y][x] = 1;
+            }else if (y+2 < field.length && x+1 < field[y].length && field[y+2][x+1] != 1){
+                y = y+2;
+                x = x+1;
                 field[y][x] = 1;
             }else if (y-1 >= 0 && x-2 >= 0 && field[y-1][x-2] != 1){
                 y = y-1;
-                x = x-2;
-                field[y][x] = 1;
-            }else if(y+1 < field.length && x-2 >= 0 && field[y+1][x-2] != 1){
-                y = y+1;
                 x = x-2;
                 field[y][x] = 1;
             }else if (y+2 < field.length && x-1 >= 0 && field[y+2][x-1] != 1){
                 y = y+2;
                 x = x-1;
                 field[y][x] = 1;
-            }else if (y+2 < field.length && x+1 < field[y].length && field[y+2][x+1] != 1){
-                y = y+2;
-                x = x+1;
+            }else if (y-2>=0 && x-1 >=0 && field[y-2][x-1] != 1){
+                y = y-2;
+                x = x-1;
+                field[y][x] = 1;
+            }else if(y+1 < field.length && x-2 >= 0 && field[y+1][x-2] != 1){
+                y = y+1;
+                x = x-2;
                 field[y][x] = 1;
             }else if (y+1 < field.length && x+2 < field[y].length && field[y+1][x+2] != 1){
                 y = y+1;
-                x = x+2;
-                field[y][x] = 1;
-            }else if (y-1 >= 0 && x+2 < field[y].length && field[y-1][x+2] != 1){
-                y = y-1;
                 x = x+2;
                 field[y][x] = 1;
             }else if (y-2 >= 0 && x+1 < field[y].length && field[y-2][x+1] != 1){
@@ -59,8 +59,14 @@ public class Main {
             System.out.println();
             for (int i = 0; i < field.length; i++) {
                 for (int j = 0; j < field[i].length; j++) {
-                    if (field[i][j] == 0)check = true;
-                    else check = false;
+                    if (field[i][j] == 0){
+                        check = true;
+                        break;
+                    }
+                    else {
+                        check = false;
+                        break;
+                    }
                 }
             }
         }
